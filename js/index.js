09da653db746580d0215a2c0fc03043edec0997a
Wendy Harris <Wendy.Harris@ibm.com>
2022-01-04 12:36:46 -0500

    hide show button

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+var resultList = $("#resultList"); 
+resultList.text("This is from jQuery");
+
+var toggleButton = $("#toggleButton");
+toggleButton.on("click", function () {
+    resultList.toggle(500);
+    
+    if (toggleButton.text() == "Hide") toggleButton.text("Show");
+    else toggleButton.text("Hide"); 
+});
+
+});
